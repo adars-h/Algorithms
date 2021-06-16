76d5cbd0e9b375391bb793b5394f36c14d3f0b65
Adarsh <adarsh333reddy@gmail.com>
2021-06-16 18:24:38 +0530

    added new file in graph-algorithms

--- graph-algorithms/cf_latokenround1_c.cpp
+++ graph-algorithms/cf_latokenround1_c.cpp
@@ -1,6 +1,5 @@
 /*
                                             C. Little Alawn's Puzzle
-
 When he's not training for IOI, Little Alawn enjoys playing with puzzles of various types to stimulate his brain.
 Today, he's playing with a puzzle that consists of a 2×n grid where each row is a permutation of the numbers 1,2,3,…,n.
 The goal of Little Alawn's puzzle is to make sure no numbers on the same column or row are the same (we'll call this
@@ -9,23 +8,19 @@ solving the puzzle many times, Little Alawn got bored and began wondering about 
 configurations of the puzzle he could achieve from an initial solved configuration only by swapping numbers in a 
 column. Unfortunately, Little Alawn got stuck while trying to solve this harder problem, so he was wondering if 
 you could help him with it. Find the answer modulo 109+7.
-
 Input
 Each test contains multiple test cases. The first line contains the number of test cases t (1≤t≤104). Description of the test cases follows.
 The first line of each test case contains a single integer n (2≤n≤4⋅105).
 The next two lines of each test case describe the initial state of the puzzle grid. Each line will be a permutation of the numbers 1,2,3,…,n and the numbers in each column and row will be pairwise distinct.
 It is guaranteed that the sum of n over all test cases does not exceed 4⋅105.
-
 Output
 For each test case output a single integer, the number of possible solved configurations of the puzzle Little Alawn can achieve from an initial solved configuration only by swapping numbers in a column. As the answer can be very large, please output it modulo 109+7.
 The answer for each test case should be on a separate line. 
-
 */
 
 // solution 
 
 /*
-
  Let's say you know whether you flipped the first cell or not, which new columns must be flipped?
  Create a graph of n nodes, draw an edge between two values if they both exist in the same column, 
  what structure does this graph have? It is a bunch of disjoint cycles (since each node has an in-degree of 2).
@@ -40,7 +35,6 @@ adjacent to it, and so on until the direction of every column in the component h
 component is a simple cycle, we will never get a contradiction). As there are 2 possible directions for any column,
 there are thus 2 ways to direct the columns in this component.Lastly, notice that the columns in each component are
 independent, so the answer is simply 2k, where k is the number of components in our graph.
-
 Time Complexity: O(n) with DFS
 */
 vector<vector<int>> arr;
